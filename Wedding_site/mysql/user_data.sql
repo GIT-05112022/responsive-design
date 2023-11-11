481de1a4649760e4aa0d4f6c5c4d4c62d6f68334
Aayush_Daghottra <79690359+GIT-05112022@users.noreply.github.com>
2023-11-11 19:27:45 +0530

    sql database creation code, modification

--- Wedding_site/mysql/user_data.sql
+++ Wedding_site/mysql/user_data.sql
@@ -1,10 +1,11 @@
-CREATE TABLE users (
+CREATE TABLE new_table (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     surname VARCHAR(50) NOT NULL,
     age DATE,
     gender ENUM('male', 'female', 'other') NOT NULL,
     contact VARCHAR(20) NOT NULL,
+    email VARCHAR(100) NOT NULL,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
